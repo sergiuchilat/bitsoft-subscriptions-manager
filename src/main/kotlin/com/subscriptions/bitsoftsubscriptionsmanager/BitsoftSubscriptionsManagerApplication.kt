31dd26fc327546eed0ea4472cc1e4562dd780f62
sergiuchilat <chilatsergiu@gmail.com>
2025-02-18 17:30:38 +0200

    Create subscriptions module

---
+++ src/main/kotlin/com/subscriptions/bitsoftsubscriptionsmanager/BitsoftSubscriptionsManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.subscriptions.bitsoftsubscriptionsmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BitsoftSubscriptionsManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<BitsoftSubscriptionsManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/subscriptions/bitsoftsubscriptionsmanager/BitsoftSubscriptionsManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.subscriptions.bitsoftsubscriptionsmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BitsoftSubscriptionsManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
